20bbf42e26ddc17a7540d6d904ba6272b995c162
Mihir2011 <102282700+Mihir2011@users.noreply.github.com>
2022-09-26 16:22:10 +0400

    Add files via upload

---
+++ RPG_Text_Game.py
@@ -0,0 +1,78 @@
+input ("Welcome to The Text RPG Game Pokemon! ")
+input ("Are you ready for your adventure ")
+input ("I hope you are Lets go! ")
+input ("Choose your starter Pokemon to Continue ")
+Choose = input ("Charmander, Bulbasaur, Squirtle ")
+if Choose == ("Charmander"):
+    CharmanderChoose = input ("Are you sure you want to choose Charmander the Fire Pokemon? Yes/No ")
+    if CharmanderChoose == ("Yes"):
+        input ("You recived Charmander the Fire pokemon")
+    if CharmanderChoose == ("No"):
+        Choose1 = input ("Bulbasaur, Squirtle ")
+        if Choose1 == ("Bulbasaur"):
+            BulbasaurChoose = input ("Are you sure you want to choose Bulbasaur the Grass Pokemon? Yes/No ")
+            if BulbasaurChoose == ("Yes"):
+                input ("You recived Bulbasaur the Grass pokemon")
+            if BulbasaurChoose == ("No"):
+                Choose2 = input ("Squirtle ")
+                if Choose2 == ("Squirtle"):
+                    input ("You recived Squirtle the Water pokemon")
+        if Choose1 == ("Squirtle"):
+            SquirtleChoose = input ("Are you sure you want to choose Squirtle the Water Pokemon? Yes/No ")
+            if SquirtleChoose == ("Yes"):
+                input ("You recived Squirtle the Water pokemon")
+            if SquirtleChoose == ("No"):
+                Choose2 = input ("Bulbasaur ")
+                if Choose2 == ("Bulbasaur"):
+                    input ("You recived Bulbasaur the Grass pokemon")
+
+if Choose == ("Bulbasaur"):
+    BulbasaurChoose1 = input ("Are you sure you want to choose Bulbasaur the Grass Pokemon? Yes/No ")
+    if BulbasaurChoose1 == ("Yes"):
+        input ("You recived Bulbasaur the Grass pokemon")
+    if BulbasaurChoose1 == ("No"):
+        Choose3 = input ("Charmander, Squirtle ")
+        if Choose3 == ("Charmander"):
+            CharmanderChoose1 = input ("Are you sure you want to choose Charmander the Fire Pokemon? Yes/No ")
+            if CharmanderChoose1 == ("Yes"):
+                input ("You recived Charmander the Fire pokemon")
+            if CharmanderChoose1 == ("No"):
+                Choose4 = input ("Squirtle")
+                if Choose4 == ("Squirtle"):
+                    input ("You recived Squirtle the Water pokemon")
+        if Choose3 == ("Squirtle"):
+            SquirtleChoose1 = input ("Are you sure you want to choose Squirtle the Water Pokemon? Yes/No ")
+            if SquirtleChoose1 == ("Yes"):
+                input ("You recived Squirtle the Water pokemon")
+            if SquirtleChoose1 == ("No"):
+                Choose4 = input ("Charmander")
+                if Choose4 == ("Charmander"):
+                    input ("You recived Charmander the Fire pokemon")
+
+if Choose == ("Squirtle"):
+    SquirtleChoose2 = input ("Are you sure you want to choose Squirtle the Water Pokemon? Yes/No ")
+    if SquirtleChoose2 == ("Yes"):
+        input ("You recived Squirtle the water pokemon")
+    if SquirtleChoose2 == ("No"):
+        Choose5 = input ("Charmander, Bulbasaur ")
+        if Choose5 == ("Charmander"):
+            CharmanderChoose2 = input ("Are you sure you want to choose Charmander the Fire Pokemon? Yes/No ")
+            if CharmanderChoose2 == ("Yes"):
+                input ("You recived Charmander the Fire pokemon")
+            if CharmanderChoose2 == ("No"):
+                Choose6 = input ("Bulbasaur")
+                if Choose6 == ("Bulbasaur"):
+                    input ("You recived Bulbasaur the Grass pokemon")
+        if Choose5 == ("Bulbasaur"):
+            BulbasaurChoose2 = input ("Are you sure you want to choose Bulbasaur the Grass Pokemon? Yes/No ")
+            if BulbasaurChoose2 == ("Yes"):
+                input ("You recived Bulbasaur the Fire pokemon")
+            if BulbasaurChoose2 == ("No"):
+                Choose6 = input ("Charmander")
+                if Choose6 == ("Charmander"):
+                    input ("You recived Charmander the Fire pokemon")
+                    
+        
+  
+
+
